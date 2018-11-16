99a9e359393b7a4fc6f8bcaa0e0ea62186d184a8
Hussain Ali Akbar <hussain94aa@gmail.com>
2018-11-16 22:27:04 +0500

    added basic tests

--- index.test.js
+++ index.test.js
@@ -1,5 +1,50 @@
+const middleware = require('./index');
+const noop = () => {};
 
+let ctx;
+describe('request trimmer', () => {
+  beforeEach(() => {
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
 
-describe('candidate service', () => {
+  test('the middleware should be defined', () => {
+    expect(middleware).toBeDefined();
+  });
 
-}
+  test('it should trim body only with default options', async () => {
+    ctx = {
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: '          invalid arg with spaces               '
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      },
+    };
+
+    const func = middleware();
+    await func(ctx, noop);
+    expect(ctx).toEqual({
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: 'invalid arg with spaces'
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      },
+    });
+  });
+});
