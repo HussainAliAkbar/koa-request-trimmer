3a8868e748c1b610d983d4170537a4c8bc043a53
Hussain Ali Akbar <hussain94aa@gmail.com>
2018-11-16 22:57:55 +0500

    added more tests

--- index.test.js
+++ index.test.js
@@ -27,7 +27,7 @@ describe('request trimmer', () => {
       },
       params: {
         arg: '       arg  with spaces         '
-      },
+      }
     };
 
     const func = middleware();
@@ -44,7 +44,227 @@ describe('request trimmer', () => {
       },
       params: {
         arg: '       arg  with spaces         '
+      }
+    });
+  });
+
+  test('it should trim body only if we pass the body argument as true to the middleware', async () => {
+    ctx = {
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: '          invalid arg with spaces               '
+        }
       },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    };
+
+    const func = middleware({
+      body: true
+    });
+    await func(ctx, noop);
+    expect(ctx).toEqual({
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: 'invalid arg with spaces'
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    });
+  });
+
+  test('it should not trim anything if we pass the body argument as false to the middleware', async () => {
+    ctx = {
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: '          invalid arg with spaces               '
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    };
+
+    const func = middleware({
+      body: false
+    });
+    await func(ctx, noop);
+    expect(ctx).toEqual({
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: '          invalid arg with spaces               '
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    });
+  });
+
+  test('it should not trim the body only (default) if we pass invalid arguments to the middleware', async () => {
+    ctx = {
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: '          invalid arg with spaces               '
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    };
+
+    const func = middleware({
+      invalidArgument: false,
+      someMoreInvalidArguments: 123,
+      yetAnotherInvalidArgument: 'abc'
+    });
+    await func(ctx, noop);
+    expect(ctx).toEqual({
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: 'invalid arg with spaces'
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    });
+  });
+  test('it should trim the body only and query if we pass relevant arguments to the middleware', async () => {
+    ctx = {
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: '          invalid arg with spaces               '
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    };
+
+    const func = middleware({
+      query: true,
+      body: true
+    });
+    await func(ctx, noop);
+    expect(ctx).toEqual({
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: 'invalid arg with spaces'
+        }
+      },
+      query: {
+        arg: 'arg  with spaces'
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    });
+  });
+  test('it should trim the body and query if we pass relevant arguments to the middleware', async () => {
+    ctx = {
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: '          invalid arg with spaces               '
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    };
+
+    const func = middleware({
+      query: true,
+      body: true
+    });
+    await func(ctx, noop);
+    expect(ctx).toEqual({
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: 'invalid arg with spaces'
+        }
+      },
+      query: {
+        arg: 'arg  with spaces'
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    });
+  });
+
+  test('it should trim the body, query and params if we pass relevant arguments to the middleware', async () => {
+    ctx = {
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: '          invalid arg with spaces               '
+        }
+      },
+      query: {
+        arg: '       arg  with spaces         '
+      },
+      params: {
+        arg: '       arg  with spaces         '
+      }
+    };
+
+    const func = middleware({
+      query: true,
+      body: true,
+      params: true
+    });
+    await func(ctx, noop);
+    expect(ctx).toEqual({
+      request: {
+        body: {
+          arg: 'valid arg',
+          anotherArg: 'invalid arg with spaces'
+        }
+      },
+      query: {
+        arg: 'arg  with spaces'
+      },
+      params: {
+        arg: 'arg  with spaces'
+      }
     });
   });
 });
